eb976257eda3a2158510c45a2d5f0c04d95da38d
John Thomas Wynter <john@Johns-Air.gateway>
2024-07-04 10:46:44 +1000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Article3 from './Article3';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Article3 />
+    </div>
+  );
+}
+
+export default App;
